71fe6a80dae143f56113f15e8c4de304dc15f20b
Bouhaddi Sabri <sabri.bouhaddi@gmail.com>
2020-03-21 03:53:15 +0100

    initial project

---
+++ src/test/java/com/sbouhaddi/sbjms/JmsSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sbouhaddi.sbjms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JmsSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
