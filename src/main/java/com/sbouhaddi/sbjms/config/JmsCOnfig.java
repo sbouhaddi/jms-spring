d37d08e61ac0c9727b2feba25752790779a24034
Bouhaddi Sabri <sabri.bouhaddi@gmail.com>
2020-03-21 04:16:08 +0100

    jms config

---
+++ src/main/java/com/sbouhaddi/sbjms/config/JmsCOnfig.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.sbouhaddi.sbjms.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import org.springframework.jms.support.converter.MessageConverter;
+import org.springframework.jms.support.converter.MessageType;
+
+/**
+ * @author bouhaddisabri
+ *
+ */
+@Configuration
+public class JmsCOnfig {
+	
+    @Bean
+    public MessageConverter messageConverter(){
+        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+        converter.setTargetType(MessageType.TEXT);
+        converter.setTypeIdPropertyName("_type");
+        return converter;
+    }
+
+}
